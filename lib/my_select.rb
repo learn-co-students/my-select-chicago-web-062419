02dc0d7ec10bd06c58cef00bf0fd2e6aca8fb129
github username <github email address>
2019-05-16 14:59:55 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,28 @@
 def my_select(collection)
- # your code here!
+    counter = 0
+    new_collection = []
+
+    while counter < collection.length
+        if yield(collection[counter]) 
+            new_collection.push(collection[counter])
+        end
+        counter += 1
+    end
+
+    new_collection
 end
+
+ 
+
+
+
+# def my_collect(array)
+#     counter = 0
+#     new_array = []
+#     while counter < array.length
+#         new_array.push(yield array[counter])
+#         counter += 1
+#     end
+
+#     new_array
+# end
