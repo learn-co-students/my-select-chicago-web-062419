43a489f2a5cceba919b46c329be14aa27bb85150
Amanda Treutler <amandathedev@gmail.com>
2019-06-11 19:14:03 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
+nums = [1, 2, 3, 4, 5]
+
 def my_select(collection)
  # your code here!
+  selected = []
+  i = 0
+  while i < collection.length
+    # http://blog.blackninjadojo.com/ruby/2019/02/09/using-select-reject-collect-inject-and-detect.html
+    if yield(collection[i])
+      selected.push(collection[i])
+    end
+    i += 1
+  end
+  selected
 end
